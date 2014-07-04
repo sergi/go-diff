8b58f93c92591f33fda6c38e2a3f70325eb9a14e
Jonathan Amsterdam <jba@google.com>
2014-07-04 07:07:34 -0400

    Added a benchmark to test a large diff.  The speedtest{1,2}.txt files are the same ones used by Neil Fraser in his speedtest code.

--- diffmatchpatch/dmp_test.go
+++ diffmatchpatch/dmp_test.go
@@ -3,6 +3,7 @@ package diffmatchpatch
 import (
 	"bytes"
 	"fmt"
+	"io/ioutil"
 	"reflect"
 	"runtime"
 	"strconv"
@@ -1340,7 +1341,7 @@ func Benchmark_DiffMain(bench *testing.B) {
 		a = a + a
 		b = b + b
 	}
-
+	bench.ResetTimer()
 	for i := 0; i < bench.N; i++ {
 		dmp.DiffMain(a, b, true)
 	}
@@ -1361,3 +1362,25 @@ func Benchmark_DiffCommonSuffix(b *testing.B) {
 		dmp.DiffCommonSuffix(a, a)
 	}
 }
+
+func Benchmark_DiffMainLarge(b *testing.B) {
+	bytes1, err := ioutil.ReadFile("speedtest1.txt")
+	if err != nil {
+		b.Fatal(err)
+	}
+	bytes2, err := ioutil.ReadFile("speedtest2.txt")
+	if err != nil {
+		b.Fatal(err)
+	}
+	s1 := string(bytes1)
+	s2 := string(bytes2)
+	dmp := New()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		dmp.DiffMain(s1, s2, true)
+	}
+}
+
+	
+	
+	

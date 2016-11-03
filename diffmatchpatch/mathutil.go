9f859138cbd2a29f28b80e709ce0bc1334c7cdec
Markus Zimmermann <markus.zimmermann@nethead.at>
2016-11-03 22:23:38 +0100

    Move util functionality into *util*.go

--- diffmatchpatch/mathutil.go
+++ diffmatchpatch/mathutil.go
@@ -1,4 +1,4 @@
-// Copyright (c) 2012 Sergi Mansilla <sergi.mansilla@gmail.com>
+// Copyright (c) 2012-2016 The go-diff authors. All rights reserved.
 // https://github.com/sergi/go-diff
 // See the included LICENSE file for license details.
 //
@@ -7,3 +7,17 @@
 // http://code.google.com/p/google-diff-match-patch/
 
 package diffmatchpatch
+
+func min(x, y int) int {
+	if x < y {
+		return x
+	}
+	return y
+}
+
+func max(x, y int) int {
+	if x > y {
+		return x
+	}
+	return y
+}
